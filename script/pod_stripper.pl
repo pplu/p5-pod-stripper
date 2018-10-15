8ff901aa1d9f249e09edbea3e39278e723aa9fee
Jose Luis Martinez <jlmartinez@capside.com>
2018-10-15 23:44:56 +0200

    Fist version of the pod stripper

---
+++ script/pod_stripper.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/env perl
+
+use v5.10;
+use warnings;
+use strict;
+use File::Find ();
+use Pod::Strip;
+use autodie;
+
+sub wanted;
+sub dostrip;
+
+use Cwd ();
+my $cwd = Cwd::cwd();
+
+# Traverse desired filesystems
+File::Find::find({wanted => \&wanted}, 'local/');
+exit;
+
+sub wanted {
+    my ($dev,$ino,$mode,$nlink,$uid,$gid);
+
+    (($dev,$ino,$mode,$nlink,$uid,$gid) = lstat($_)) &&
+    -f _ &&
+    /^.*\.pm\z/s &&
+    dostrip($_);
+}
+
+
+sub dostrip {
+    my $file = shift;
+    say "Stripping $file";
+
+    my $strip = Pod::Strip->new;
+    my $module;
+    { local $/ = undef;
+      open my $pm, '<', $file;
+      $module = <$pm>;
+      close $pm
+    }
+
+    # We unlink the original pm
+    unlink $file;
+
+    open my $pm, '>', $file;
+    $strip->output_fh($pm);
+    $strip->parse_string_document($module);
+    close $pm;
+}
+
